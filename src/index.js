6c39f583a4a7505eaf7b520b4151b4d41c670399
Osato <osatohanmenogbeide1@gmail.com>
2024-11-30 16:21:37 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '@mysten/dapp-kit/dist/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui.js/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AppProvider } from './component/context';
+
+const queryClient = new QueryClient();
+const networks = {
+	localnet: { url: getFullnodeUrl('localnet') },
+	devnet: { url: getFullnodeUrl('devnet') },
+	testnet: { url: getFullnodeUrl('testnet') },
+	mainnet: { url: getFullnodeUrl('mainnet') },
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<QueryClientProvider client={queryClient}>
+		<SuiClientProvider networks={networks} defaultNetwork="testnet">
+			<WalletProvider>
+				<BrowserRouter>
+					<AppProvider>
+						<App />
+					</AppProvider>
+				</BrowserRouter>
+			</WalletProvider>
+		</SuiClientProvider>
+	</QueryClientProvider>,
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
